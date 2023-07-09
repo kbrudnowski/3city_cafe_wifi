53d13d5c40e186230afbd370bba67069eaef5b74
kbrudnowski <krzysztof.brudnowski@gmail.com>
2023-07-09 16:03:24 +0200

    cafes model added

--- website/cafes/models.py
+++ website/cafes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Cafes(models.Model):
+    photo = models.CharField(max_length = 400)
+    name = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 400)
+    g_rating = models.DecimalField(max_digits = 3, decimal_places = 2)
+    work_conditions = models.DecimalField(max_digits = 2, decimal_places = 1)
+
+    def __str__(self):
+        return self.name
